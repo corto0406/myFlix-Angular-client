7448401280adf8506936f043e686de757118c7e4
corto0406 <132603219+corto0406@users.noreply.github.com>
2024-01-02 23:36:01 +0100

    immersion 6.4

---
+++ src/polyfills.ts
@@ -0,0 +1,16 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE9, IE10, IE11, and last 2 versions of major browsers */
+import 'core-js/es6';
+
+/** IE10 and IE11, and last 2 versions of major browsers on the ESR */
+import 'core-js/es7/reflect';
+
+/** Zone JS is required by Angular itself. */
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
